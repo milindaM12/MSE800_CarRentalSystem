c170d70442513a53ca50907d48b03e1a0b298c36
milindaM12 <milindamanamperi@gmail.com>
2026-05-09 20:39:53 +1200

    commit for carService

--- models/car.py
+++ models/car.py
@@ -1,4 +1,62 @@
-class Car:
-    def __init__(self, car_id, brand):
-        self.car_id = car_id
-        self.brand = brand
+from models.vehicle import Vehicle
+
+class Car(Vehicle):
+    def __init__(self, car_id, brand, model, year, mileage, price_per_day, min_days, max_days):
+        super().__init__(car_id, brand, model, price_per_day)
+        self._year = year
+        self._mileage = mileage
+        self._min_days = min_days
+        self._max_days = max_days
+        self.__is_available = True
+
+    def get_details(self):
+        status = "Available" if self.__is_available else "Not Available"
+        return (
+            f"ID: {self._vehicle_id} | {self._brand} {self._model} ({self._year}) | "
+            f"Mileage: {self._mileage} | ${self._price_per_day}/day | "
+            f"Min:{self._min_days} Max:{self._max_days} | {status}"
+        )
+
+    def is_available(self):
+        return self.__is_available
+
+    def get_price(self):
+        return self._price_per_day
+
+    def get_min_days(self):
+        return self._min_days
+
+    def get_max_days(self):
+        return self._max_days
+    
+    def rent(self):
+        if self.__is_available:
+            self.__is_available = False
+            return True
+        return False
+
+    def return_car(self):
+        self.__is_available = True
+
+    def to_dict(self):
+        return {
+            "id": self._vehicle_id,
+            "brand": self._brand,
+            "model": self._model,
+            "year": self._year,
+            "mileage": self._mileage,
+            "price": self._price_per_day,
+            "min_days": self._min_days,
+            "max_days": self._max_days,
+            "available": self.__is_available
+        }
+
+    @staticmethod
+    def from_dict(data):
+        car = Car(
+            data["id"], data["brand"], data["model"],
+            data["year"], data["mileage"],
+            data["price"], data["min_days"], data["max_days"]
+        )
+        car.__is_available = data["available"]
+        return car
--- services/car_service.py
+++ services/car_service.py
@@ -1,2 +1,104 @@
+from data.db import get_connection
+from models.car import Car
+
 class CarService:
-    pass
+    
+    def get_car_by_id(self, car_id):
+        conn = get_connection()
+        cursor = conn.cursor()
+
+        cursor.execute("SELECT * FROM cars WHERE id=?", (car_id,))
+        r = cursor.fetchone()
+
+        conn.close()
+
+        if not r:
+            return None
+
+        car = Car(
+            r[0], r[1], r[2],
+            r[3], r[4],
+            r[5], r[6], r[7]
+        )
+
+        if r[8] == 0:
+            car.rent()
+
+        return car
+
+    def add_car(self, car):
+        conn = get_connection()
+        cursor = conn.cursor()
+
+        cursor.execute("""
+        INSERT INTO cars VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+        """, (
+            car.get_id(),
+            car._brand,
+            car._model,
+            car._year,
+            car._mileage,
+            car._price_per_day,
+            car._min_days,
+            car._max_days,
+            1
+        ))
+
+        conn.commit()
+        conn.close()        
+
+    def get_all_cars(self):
+
+        conn = get_connection()
+        cursor = conn.cursor()
+
+        cursor.execute("SELECT * FROM cars")
+        rows = cursor.fetchall()
+
+        if not rows:
+            print("No cars available.")
+            return
+
+        for r in rows:
+            car = Car(
+                r[0],  # id
+                r[1],  # brand
+                r[2],  # model
+                r[3],  # year
+                r[4],  # mileage
+                r[5],  # price
+                r[6],  # min_days
+                r[7]   # max_days
+            )
+
+            # set availability manually
+            if r[8] == 0:
+                car.rent()  # mark as not available
+
+            print(car.get_details())
+
+        conn.close()
+
+
+    def update_availability(self, car_id, available):
+        conn = get_connection()
+        cursor = conn.cursor()
+
+        cursor.execute(
+            "UPDATE cars SET available=? WHERE id=?",
+            (1 if available else 0, car_id)
+        )
+
+        conn.commit()
+        conn.close()
+
+ 
+    def delete_car(self, car_id):
+        conn = get_connection()
+        cursor = conn.cursor()
+
+        cursor.execute("DELETE FROM cars WHERE id=?", (car_id,))
+
+        conn.commit()
+        conn.close()
+        print("Car deleted")
