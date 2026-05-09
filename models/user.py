1ff5586830093e9f570983e8890d3baf0f4dc7e5
milindaM12 <milindamanamperi@gmail.com>
2026-05-10 06:09:11 +1200

    commit for userService

--- models/user.py
+++ models/user.py
@@ -1,3 +1,25 @@
 class User:
-    def __init__(self, username):
-        self.username = username
+    def __init__(self, username, password, role):
+        self._username = username
+        self._password = password
+        self._role = role  # admin or customer
+
+    def get_username(self):
+        return self._username
+
+    def get_role(self):
+        return self._role
+
+    def check_password(self, password):
+        return self._password == password
+
+    def to_dict(self):
+        return {
+            "username": self._username,
+            "password": self._password,
+            "role": self._role
+        }
+
+    @staticmethod
+    def from_dict(data):
+        return User(data["username"], data["password"], data["role"])
--- services/user_service.py
+++ services/user_service.py
@@ -1,2 +1,52 @@
+from models.user import User
+from data.db import get_connection
+
+
 class UserService:
-    pass
+    # def __init__(self, file_path="data/users.json"):
+    #     self.file_path = file_path
+    #     self.users = self.load_users()
+
+    def load_users(self):
+        try:
+            with open(self.file_path, "r") as f:
+                return [User.from_dict(u) for u in json.load(f)]
+        except:
+            return []
+
+    def save_users(self):
+        with open(self.file_path, "w") as f:
+            json.dump([u.to_dict() for u in self.users], f, indent=4)
+
+    def register(self, username, password, role):
+        conn = get_connection()
+        cursor = conn.cursor()
+
+        cursor.execute("SELECT * FROM users WHERE username=?", (username,))
+        if cursor.fetchone():
+            print("User already exists!")
+            conn.close()
+            return None
+
+        cursor.execute(
+            "INSERT INTO users VALUES (?, ?, ?)",
+            (username, password, role)
+        )
+        conn.commit()
+        conn.close()
+        return User(username, password, role)
+
+    def login(self, username, password):
+        conn = get_connection()
+        cursor = conn.cursor()
+
+        cursor.execute(
+            "SELECT * FROM users WHERE username=? AND password=?",
+            (username, password)
+        )
+        row = cursor.fetchone()
+        conn.close()
+
+        if not row:
+            return None
+        return User(row[0], row[1], row[2])
